8049db145881e335ff6fe958e9e86656be0efa93
Anton dela Cruz <adcruzer02@gmail.com>
2026-02-12 14:54:54 -0500

    Updated users.js
    added users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
+
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// Map the endpoints
+router.get('/', userController.getAllUsers);
+router.post('/', userController.createUser);
 
 module.exports = router;
